f2fc1ad3743898994b49c6ebeec6ffae92e6c8c3
Jennifer_Haase <jhaase@ucsd.edu>
2018-07-24 14:59:55 -0700

    added swversion.h

---
+++ root/roc/roc/swversion.h
@@ -0,0 +1,12 @@
+﻿/// \copyright 2017 David Jabson, Brainstorm Engineering
+///
+/// \file swversion.h
+/// \brief Header file for tracking versions
+///    
+
+
+
+// Definitions
+#define SWDate					"20180722,2225"		///< Build date and time
+#define SWVersion				"026"				///< Software version number
+#define ZephVersion				"100"				///< Zephyr protocol version
